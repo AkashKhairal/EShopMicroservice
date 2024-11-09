2532aa92cd4a0a84f2c61caef30a81f6713770b7
akash <akash@DESKTOP-03IJ2IH>
2024-11-09 23:32:07 +0530

    GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+
+    public record GetProuctByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProuctByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProuctByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+
+            var products = await session.Query<Product>()
+                .Where(p=> p.Category.Contains(query.Category))
+                .ToListAsync();
+
+            if (products is null)
+            {
+                throw new  ProductNotFoundException();
+            }
+
+
+            return new GetProductByCategoryResult(products);
+
+        }
+    }
+}
